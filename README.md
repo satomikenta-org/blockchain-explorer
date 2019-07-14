848d6d4f87dd83219159ccf405f00572a5a41454
里見健太 <satomikennta1213@gmail.com>
2019-07-14 13:18:37 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+@ Development
+start next.js and eth-listener
+$ npm run dev
+
+start only next.js 
+$ npm run dev-server
+
+
+.env example 
+```
+MYSQL_HOST=""
+MYSQL_USER=""
+MYSQL_PASSWORD=""
+MYSQL_PORT=""
+MYSQL_DATABASE=""
+CONTRACT_ADDRESS="0x..."
+GETH_URL="http://...:8545"
+
+```
+
---
+++ core/index.js
@@ -0,0 +1,28 @@
+const TxLog = require('./tx_log');
+const MySQL = require('./db/mysql');
+const { makeEventWatcher } = require('./ethUtils');
+const { makeRepo } = require('./repo');
+
+// configure Data Store.
+const db = new MySQL();
+const repo = makeRepo(db);
+
+// configure CallBack to be triggered.
+const transferEventCallback = async (log) => {
+  try {
+    const transferLog = TxLog.build(log).make();
+    await repo.saveTx(transferLog);
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+console.log("Eth-Listener Start");
+// configure EventWatcher
+makeEventWatcher('Transfer', transferEventCallback); 
+
+
+
+
+
+
